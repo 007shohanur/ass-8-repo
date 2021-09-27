88266b0fc9702673c106219bef7e011f4a263335
007shohanur <shohanur007rm@gmail.com>
2021-09-27 22:05:00 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header_bg ">
+            <div className="transparent text-center text-success text-bold">
+            <h2>Dhaka Medical College and Hospital</h2>
+            <p>
+            The college's original building was built before the Partition of Bengal of 1905. As of 1904, the building was being used as the secretariat (headquarters) of the newly formed provinces of East Bengal and Assam.
+            </p>
+            <h2>Total Students : 1.5 Million</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/fakeData/fakedata.js
@@ -0,0 +1,111 @@
+const fackData = [
+    {   id: 1,
+        name: 'Dr. Md. Monirul Islam',
+         designation: 'Profrssor', 
+         phone: +8801716363505,
+         email: 'monirul.islam@du.ac.bd', 
+         salary: 50000,
+         img: 'https://i.ibb.co/1GqsVzq/1457435990.jpg'
+    },
+    {
+        id: 2,
+        name: 'Dr. Md. Ghulam Mustafa',
+         designation: 'Profrssor', 
+         phone: +880-2-9661920/7778,
+         email: 'mghulam@du.ac.bd', 
+         salary: 60000,
+         img: 'https://i.ibb.co/3fC3fsv/1457438772.jpg'
+    },
+    {   
+        id: 3,
+        name: 'Dr. Mahmud Hasan',
+         designation: 'Profrssor', 
+         phone: +880-2-9661920/7775,
+         email: 'mhasan@du.ac.bd', 
+         salary: 70000,
+         img: 'https://i.ibb.co/mhxgVb5/1457498581.jpg'
+    },
+    {
+        id: 4,
+        name: 'Dr. Kaniz Fatema',
+         designation: 'Profrssor', 
+         phone:+880-2-9661920/7785,
+         email: 'kaniz@du.ac.bd', 
+         salary: 80000,
+         img: 'https://i.ibb.co/2SHC72M/1457435009.jpg'
+        
+    },
+    { 
+        id: 5,
+        name: 'Dr. Md. Golam Rabbane',
+         designation: 'Profrssor', 
+         phone: +88001818229810,
+         email: 'rabbane@du.ac.bd', 
+         salary: 90000,
+         img: 'https://i.ibb.co/yyJzKSc/1457271298.jpg'
+    },
+    {
+        id: 6,
+        name: 'Dr. Mohammad Raknuzzaman',
+         designation: 'Profrssor', 
+         phone: +8801712054454,
+         email: 'mraknuz@du.ac.bd', 
+         salary: 100000,
+         img: 'https://i.ibb.co/1rBGkGq/1459922501.jpg'
+    },
+    { 
+        id: 7,
+        name: 'Dr Mohammad Shamsur Rahman',
+         designation: 'Profrssor', 
+         phone: +8801818314899,
+         email: 'shamsur@du.ac.bd', 
+         salary: 1100000,
+         img: 'https://i.ibb.co/SdT4DP5/1459839361.jpg'
+    },
+    {
+        id: 8,
+        name: 'Mrs. Wahida Haque',
+         designation: 'Profrssor', 
+         phone: +880-2-9661920/7775,
+         email: 'whaque_du@yahoo.com', 
+         salary: 1200000,
+         img: 'https://i.ibb.co/Fh183bp/1460785884.jpg'
+    },
+    {
+        id: 9,
+        name: 'Mohammad Mamun Chowdhury',
+         designation: 'Profrssor', 
+         phone: +8801713409375,
+         email: 'mmchowdhury@du.ac.bd', 
+         salary: 1300000,
+         img: 'https://i.ibb.co/6BLNSxr/1460785983.jpg'
+    },
+    {
+        id: 10,
+        name: 'Dr. Anwar Hossain',
+         designation: 'Profrssor', 
+         phone: +8801972198680,
+         email: 'ahossain@du.ac.bd', 
+         salary: 1400000,
+         img: 'https://i.ibb.co/6bYnygC/1459660141.jpg'
+    },
+    {
+        id: 11,
+        name: 'Dr. Mosammat Salma Akter',
+         designation: 'Profrssor', 
+         phone: +880-2-9661920,
+         email: 'sakter@du.ac.bd', 
+         salary: 150000,
+         img: 'https://i.ibb.co/1rTSkz5/1457501635.jpg'
+    },
+    {
+        id: 12,
+        name: 'Dr. M. Shahneawz Khan',
+         designation: 'Profrssor', 
+         phone: 880-2-9661920,
+         email: 'shahneawz@du.ac.bd', 
+         salary: 160000,
+         img: 'https://i.ibb.co/c2yzyBv/1457503182.jpg'
+    },
+]
+export default fackData;
